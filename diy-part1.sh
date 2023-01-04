d553493c0efb4b67b53bb8880f6abe61b3059da1
snove <70341992+snove520@users.noreply.github.com>
2023-01-04 23:08:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,7 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*kenzo\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*kenzo\)/\1/' feeds.conf.default
 
 # Add a feed source
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
